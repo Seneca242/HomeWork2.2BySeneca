68fdf6ececd505da44eb4dd912232c08dc36484e
Seneca <flabbergasted242@gmail.com>
2021-07-10 23:24:58 +0300

    Initial Commit

---
+++ HomeWork2.2BySeneca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.2BySeneca
+//
+//  Created by Дмитрий Дмитрий on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
